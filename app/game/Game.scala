762cb976eb568bbf61aea9766e4f556580abbf5c
Andrzej Stasiak <andst4@gmail.com>
2015-06-08 15:23:23 +0200

    Development of Game object

--- app/game/Game.scala
+++ app/game/Game.scala
@@ -1,17 +1,60 @@
 package game
 
-class Game {
-  val playerA: PlayerData = new PlayerData()
-  val playerB: PlayerData = new PlayerData()
+case class GameParams(size: (Int, Int), walls: Int)
+case object GameState extends Enumeration {
+  val Awaiting, Ongoing, Finished = Value
+}
+
+class Game(val params: GameParams) {
+  var playerA: Option[PlayerData] = None
+  var playerB: Option[PlayerData] = None
+  var currentPlayerA: Boolean = true
+  var winnerPlayerA: Option[Boolean] = None
+  var gameState: GameState.Value = GameState.Awaiting
+  
+  def privatizeBoard(board: Board): Board = board // TODO
+  
+  def sendStates() = {
+    playerA.map {
+      val gs: GameStatus = GameStatus(currentPlayerA,
+          (playerA.map(_.board).flatten, playerB.map(_.board).flatten.map(privatizeBoard _)),
+          gameState, winnerPlayerA)
+      _.callbacks.updateStatus(gs)
+    }
+    playerB.map {
+      val gs: GameStatus = GameStatus(!currentPlayerA,
+          (playerA.map(_.board).flatten.map(privatizeBoard _), playerB.map(_.board).flatten),
+          gameState, winnerPlayerA.map(!_))
+      _.callbacks.updateStatus(gs)
+    }
+  }
+  
+  def sitDown(playerName: String, callbacks: Callbacks): Option[String] = (playerA, playerB) match {
+    case (None,_) => { playerA = Some(new PlayerData(callbacks, playerName)); Some("A") }
+    case (Some(_), None) => { playerB = Some(new PlayerData(callbacks, playerName)); Some("B") }
+    case (Some(_), Some(_)) => None
+  }
+  
+  def initBoard(playerId: String, board: Board) = (playerId match {
+    case "A" => playerA
+    case "B" => playerB
+    case _ => None
+  }).map(p=>{
+    p.board = Some(board);
+    if(playerA.flatMap(_.board).isDefined && playerB.flatMap(_.board).isDefined) {
+      gameState = GameState.Ongoing;
+      sendStates();
+    }
+  })
 }
 
 
-class PlayerData {
-  var board: Board = _
-  var callbacks: Callbacks = _
-  var name: String = _
+class PlayerData(val callbacks: Callbacks, val name: String) {
+  var board: Option[Board] = None
 }
+
+case class GameStatus(yourMove: Boolean, boards: (Option[Board], Option[Board]), gameStatus: GameState.Value, youWon: Option[Boolean])
 
 trait Callbacks {
-  
+  def updateStatus(status: GameStatus)
 }
--- app/ws.sc
+++ app/ws.sc
@@ -5,4 +5,6 @@ object ws {
   val x = Set(s1, s2, s3)                         //> x  : scala.collection.immutable.Set[Option[String]] = Set(Some(hello), Some(
                                                   //| world), None)
   val xx = x.flatMap(x=>x)                        //> xx  : scala.collection.immutable.Set[String] = Set(hello, world)
+  val xxx = x.flatten                             //> xxx  : scala.collection.immutable.Set[String] = Set(hello, world)
+  val sss = List(s1,s2).flatten                   //> sss  : List[String] = List(hello, world)
 }
